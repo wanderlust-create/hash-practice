bcc70a47ab1590f25eacfe9509cb51f84f30926a
Tamara Dowis <tamara.dowis@gmail.com>
2021-09-23 20:50:18 -0500

    Add animal hash

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,10 +1,20 @@
 car =
 {
-  "volkswagon" = 'jetta'
-  "honda" = 'accord'
-  "bently" = 'expensive',
-  "toyota" = 'why',
-  "happiness" = 100,
-  "sadness" = 101,
-  
+  "volkswagon": 'jetta'
+  "honda": 'accord'
+  "bently": 'expensive',
+  "toyota": 'why',
+  "happiness": 100,
+  "sadness": 101,
+
 }
+p car
+
+animals = {
+  "lions": 17,
+  "zebras": 'tom, lucy, and harry',
+  "fishes": ['star', 'angel', 'beta'],
+  "is_open" = false,
+
+}
+p animals
